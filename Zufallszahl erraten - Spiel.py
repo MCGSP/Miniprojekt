76e3720374d36706cb9fa1362632ff9a9b93a64a
MCGSP <stratomeierpleynet@outlook.com>
2025-04-10 08:38:47 +0200

    Update Zufallszahl erraten - Spiel.py

--- Zufallszahl erraten - Spiel.py
+++ Zufallszahl erraten - Spiel.py
@@ -3,15 +3,15 @@
 import random
 zahl = random.randint(1, 100)
 versuch = int(input("Ich habe mir eine Zahl von 1 bis 100 ausgedacht. Du hast 10 Versuche, um sie zu erraten: "))
-versuchsanzahl = 9
-while versuchsanzahl > 0:
+versuchsanzahl = 1
+while versuchsanzahl < 10:
     if versuch < zahl:
         versuch = int(input("Zu niedrig, versuch es nochmal: "))
-        versuchsanzahl = versuchsanzahl - 1
+        versuchsanzahl = versuchsanzahl + 1
     if versuch > zahl:
         versuch = int(input("Zu hoch, versuch es nochmal: "))
-        versuchsanzahl = versuchsanzahl - 1
+        versuchsanzahl = versuchsanzahl + 1
     if versuch == zahl:
-        print("Du hast meine Zahl erraten!")
+        print("Du hast meine Zahl in " + str(versuchsanzahl) + " Versuchen erraten!")
         
 print("Du hast keine Versuche mehr übrig.")
